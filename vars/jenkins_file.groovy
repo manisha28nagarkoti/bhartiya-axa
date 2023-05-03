ad2a8c3be15634a8957c83d1f96ed778dd380b68
manisha28nagarkoti <126867552+manisha28nagarkoti@users.noreply.github.com>
2023-05-03 17:41:57 +0530

    Update jenkins_file.groovy

--- vars/jenkins_file.groovy
+++ vars/jenkins_file.groovy
@@ -10,10 +10,10 @@ pipeline{
    
     environment{
       git_url="git@github.com:manisha28nagarkoti/${service-name}.git"
-      docker_repo="337764066236.dkr.ecr.ap-south-1.amazonaws.com"
-      devops_git_url='git@gitlab.intelligrape.net:bharti-axa/devops.git'
+      docker_registry = "337764066236.dkr.ecr.ap-south-1.amazonaws.com"
+      //devops_git_url='git@gitlab.intelligrape.net:bharti-axa/devops.git'
 
-      image_name= "${docker_repo}/${service_name}"
+      docker_repo= "${docker_registry}/${service-name}"
       
       credential_git='jenkin-slave-git'
       env.APPLICATION="${JOB_BASE_NAME}"
@@ -25,11 +25,11 @@ pipeline{
    
     stages{
 
-     stage('notifier'){
-       steps{
-         myObject.Notifier()
-       }
-     }
+//      stage('notifier'){
+//        steps{
+//          myObject.Notifier()
+//        }
+//      }
     
 
      stage('Clean WorkSpace'){
@@ -49,38 +49,38 @@ pipeline{
         
        }
   }
-   stage('Gaining Access for deployment') {
-    steps{
-     myObject.Gaining_access_for_ecr()
+//    stage('Gaining Access for deployment') {
+//     steps{
+//      myObject.Gaining_access_for_ecr()
 
-    }
-  }
+//     }
+//   }
   stage('ECR Login'){
       steps{
-        myObject.Ecr_login(region,docker_repo)}
+        myObject.Ecr_login(region,docker_registry)}
   }
 stage('Build Docker Image') {
       steps{
-        myObject.Build_Docker_Image(image_name)}
+        myObject.Build_Docker_Image(docker_repo)}
   }
   
 stage('Push Image to ECR'){
       steps{
-        myObject.Push_Image_to_ECR(docker_repo,service_name,tag,image_name)}
+        myObject.Push_Image_to_ECR(docker_repo,docker_registry)}
   }
    
 stage('Image cleanup'){
      steps{
-       myObject.Image_cleanup(docker_repo,image_name,tag)}
+       myObject.Image_cleanup()}
       
   }
 
-  stage('helm update'){
-    step{
-      myObject.Helm_update(devops_git_url,)
-    }
+//   stage('helm update'){
+//     step{
+//       myObject.Helm_update(devops_git_url,service_name)
+//     }
       
-  }
+//   }
  }
 }
 
